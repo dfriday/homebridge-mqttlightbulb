7013b3348196691ca9d3cf212aac582b63588cde
dfriday <dfriday0815@gmail.com>
2017-04-01 13:27:05 +0200

    convert rgb hsv

--- index.js
+++ index.js
@@ -174,3 +174,83 @@ mqttlightbulbAccessory.prototype.setSaturation = function(saturation, callback, 
 mqttlightbulbAccessory.prototype.getServices = function() {
   return [this.service];
 }
+
+
+
+/**
+ * Converts an HSV color value to RGB. Conversion formula
+ * adapted from https://en.wikipedia.org/wiki/HSL_and_HSV
+ * Assumes h, s, and v are contained in the set [0, 1] and
+ * returns rgb (FFFFFF)
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  v       The value (brigthness)
+ * @return  HexNumber       The RGB representation
+ */
+ 
+Utils.hsv2rgb = function (h, s, v) {
+
+  //console.log("h: %s s: %s v: %s", h, s, v);
+  var r, g, b;
+  
+  if( s == 0 ) {
+    r = v; g = v; b = v;
+  }
+  else {
+    var i = Math.floor(h * 6);
+    var f = h * 6 - i;
+    var p = v * (1 - s);
+    var q = v * (1 - f * s);
+    var t = v * (1 - (1 - f) * s);
+
+    switch(i % 6){
+      case 0: r = v; g = t; b = p; break;
+      case 1: r = q; g = v; b = p; break;
+      case 2: r = p; g = v; b = t; break;
+      case 3: r = p; g = q; b = v; break;
+      case 4: r = t; g = p; b = v; break;
+      case 5: r = v; g = p; b = q; break;
+    }
+  }
+  r = Math.round(r*255);
+  g = Math.round(g*255);
+  b = Math.round(b*255);
+  //console.log("r: %s g: %s b: %s", r, g, b);
+  return Number(0x1000000 + r*0x10000 + g*0x100 + b).toString(16).substring(1).toUpperCase();
+}
+
+/**
+ * Converts an RGB color value to HSV. Conversion formula
+ * adapted from https://en.wikipedia.org/wiki/HSL_and_HSV
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and v in the set [0, 1].
+ *
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @return  Array           The HSV representation
+ */
+ 
+Utils.rgb2hsv = function(r, g, b) {
+
+  r = r/255, g = g/255, b = b/255;
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, v = max;
+
+  var d = max - min;
+  s = max == 0 ? 0 : d / max;
+
+  if(max == min){
+      h = 0; // achromatic
+  } else {
+      switch(max){
+          case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+          case g: h = (b - r) / d + 2; break;
+          case b: h = (r - g) / d + 4; break;
+      }
+      h /= 6;
+  }
+
+  return [h, s, v];
+}
